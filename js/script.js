1ff71531ec6d88af0b4b498db06596eab9e0b3b3
victoria <v.suprun1961@gmail.com>
2025-11-09 19:40:40 +0200

    add functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 const contents = document.querySelectorAll('.program-line__content');
 
-
 contents.forEach((el) => {
-    const title = el.querySelector('.program-line__title');
-    const descr = el.querySelector('.program-line__descr');
+  const title = el.querySelector('.program-line__title');
+  const descr = el.querySelector('.program-line__descr');
 
-    title.addEventListener('click', () => {
-    descr.classList.toggle('active');
+  title.addEventListener('click', () => {
+    contents.forEach((item) => {
+      const itemDescr = item.querySelector('.program-line__descr');
+      itemDescr.classList.remove('active');
+    });
+
+    descr.classList.add('active');
+  });
 });
 
-});
+
+
+
+
