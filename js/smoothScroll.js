dfd8c1d63504280a54b9ac05693f58680c42e0d2
victoria <v.suprun1961@gmail.com>
2025-11-10 04:41:36 +0200

    add functionality

---
+++ js/smoothScroll.js
@@ -0,0 +1,26 @@
+const navbar = document.querySelector('.header__nav');
+console.log(navbar);
+const navLinks = navbar.querySelectorAll('a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(e.target);
+        console.log(link.getAttribute('href'));
+        console.log(document.querySelector(link.getAttribute('href')));
+        const section = document.querySelector(link.getAttribute('href'));
+        if (section) {
+            console.log('scroll');
+            seamless.scrollIntoView(section, {
+                block: 'start',
+                behavior: 'smooth',
+                inline: 'center'
+            });
+            }
+        
+        // const id = link.getAttribute('href');
+        // document.querySelector(id).scrollIntoView({
+        //     behavior: 'smooth'
+        // });
+    });
+});
